541770fc359707dfb6266824ed0e84f6a36b7b7b
NycollSM <nycollsotomorales21@gmail.com>
2018-08-05 02:05:21 -0600

    uno

---
+++ uno.js
@@ -0,0 +1,8 @@
+//1. busca el  primer elemento que tenga un id, clase o selector para css y se modifica el css
+//2. solo lo aplica AL PRIMER ELEMENTO , 
+//3. retorna una propiedad de css al primer el elemento que contiene una clase o id 
+document.querySelector("#qs").style.color = 'yellow';
+console.log('#qs');
+
+
+//queryselerctorAll crea un array (nodelist) con  los elementos de un ul y la clase o id que se le asigno 
